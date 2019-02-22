14b10ad2b8c5990033c6a3d80a55639abceb8dd2
Chetan Ruparel <chetan@Chetans-iMac.local>
2019-02-22 15:44:16 +0530

    added model

--- my-app/src/BottomBar.js
+++ my-app/src/BottomBar.js
@@ -5,6 +5,8 @@ import Paper from 'material-ui/Paper';
 import IconLocationOn from 'material-ui/svg-icons/communication/location-on';
 import IconMenuExampleControlled from './PopOver'
 import RaisedButton from 'material-ui/RaisedButton';
+import TodoItems from './todoItems'
+import SimpleModalWrapped from './model.js'
 
 
 const recentsIcon = <FontIcon className="material-icons">restore</FontIcon>;
@@ -23,15 +25,21 @@ class BottomNavigationExampleSimple extends Component {
 
   select = (index) => this.setState({selectedIndex: index});
 
+  addTask() {
+ console.log('adding task');
+
+  };
+
   render() {
     const style = {
+    };
 
-};
+
     return (
       <Paper zDepth={1}>
         <BottomNavigation selectedIndex={this.state.selectedIndex}>
           <IconMenuExampleControlled />
-          <RaisedButton  label="Add a new task" primary={true} style={style} />
+          <SimpleModalWrapped />
           <IconMenuExampleControlled />
         </BottomNavigation>
       </Paper>
---
+++ my-app/src/model.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
+import RaisedButton from 'material-ui/RaisedButton';
+import TodoList from './todoList'
+import TodoItems from './todoItems'
+
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: 'none',
+  },
+});
+
+class SimpleModal extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  inputElement = React.createRef()
+  constructor() {
+    super()
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: '',
+      },
+    }
+  }
+  deleteItem = key => {
+    const filteredItems = this.state.items.filter(item => {
+      return item.key !== key
+    })
+    this.setState({
+      items: filteredItems,
+    })
+  }
+
+  handleInput = e => {
+    const itemText = e.target.value
+    const currentItem = { text: itemText, key: Date.now() }
+    this.setState({
+      currentItem,
+    })
+  }
+
+  addItem = e => {
+    e.preventDefault()
+    const newItem = this.state.currentItem
+    if (newItem.text !== '') {
+      const items = [...this.state.items, newItem]
+      this.setState({
+        items: items,
+        currentItem: { text: '', key: '' },
+      })
+    }
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    const style = {
+    };
+
+    return (
+      <div>
+
+
+        <RaisedButton onClick={this.handleOpen} label="Add a new task" primary={true} style={style} />
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <div style={getModalStyle()} className={classes.paper}>
+            <Typography variant="h6" id="modal-title">
+              Task
+            </Typography>
+            <Typography variant="subtitle1" id="simple-modal-description">
+              Write your task here
+            </Typography>
+            <TodoList
+              addItem={this.addItem}
+              inputElement={this.inputElement}
+              handleInput={this.handleInput}
+              currentItem={this.state.currentItem}
+            />
+            <TodoItems entries={this.state.items} deleteItem={this.deleteItem} />
+
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+SimpleModal.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+// We need an intermediary variable for handling the recursive nesting.
+const SimpleModalWrapped = withStyles(styles)(SimpleModal);
+
+export default SimpleModalWrapped;
